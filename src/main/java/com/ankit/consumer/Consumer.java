b05e73f38738ee45af5d700b941da7d6d63df83c
ankghild <aghildiyal@sapient.com>
2020-06-23 20:41:21 -0400

    Kafka consumer

---
+++ src/main/java/com/ankit/consumer/Consumer.java
@@ -0,0 +1,36 @@
+package com.ankit.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Component
+public class Consumer {
+
+    @PostConstruct
+    public void read(){
+
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9093,localhost:9094");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Arrays.asList("my-replicated-topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
+}
